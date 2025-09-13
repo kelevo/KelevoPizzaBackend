22b0884805ffc27c1d11ab7c5006c722c55a077e
Patrick Jhonatan Hernandez Blanco <patrickjhonatanh@gmail.com>
2025-09-13 16:04:55 -0600

    Project structure complete

---
+++ src/main/java/com/kelevo/pizza/KelevoPizzaApplication.java
@@ -0,0 +1,13 @@
+package com.kelevo.pizza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KelevoPizzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KelevoPizzaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kelevo/pizza/KelevoPizzaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kelevo.pizza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KelevoPizzaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
