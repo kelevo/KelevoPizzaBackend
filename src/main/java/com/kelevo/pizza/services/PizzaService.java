8ec0e83e10d33dc38b676255a0350d74fe0aaf52
Patrick Jhonatan Hernandez Blanco <patrickjhonatanh@gmail.com>
2025-09-25 17:56:35 -0600

    Filter by ingredient with query methods

--- src/main/java/com/kelevo/pizza/services/PizzaService.java
+++ src/main/java/com/kelevo/pizza/services/PizzaService.java
@@ -31,6 +31,14 @@ public class PizzaService {
         return this.pizzaRepository.findAllByAvailableTrueAndNameIgnoreCase(name);
     }
 
+    public List<PizzaEntity> getWith(String ingredient) {
+        return this.pizzaRepository.findAllByAvailableTrueAndDescriptionContainingIgnoreCase(ingredient);
+    }
+
+    public List<PizzaEntity> getWithout(String ingredient) {
+        return this.pizzaRepository.findAllByAvailableTrueAndDescriptionNotContainingIgnoreCase(ingredient);
+    }
+
     public PizzaEntity save(PizzaEntity pizza) {
         return this.pizzaRepository.save(pizza);
     }
--- src/main/java/com/kelevo/pizza/web/controller/PizzaController.java
+++ src/main/java/com/kelevo/pizza/web/controller/PizzaController.java
@@ -35,6 +35,16 @@ public class PizzaController {
         return ResponseEntity.ok(this.pizzaService.getByName(name));
     }
 
+    @GetMapping("/with/{ingredient}")
+    public ResponseEntity<List<PizzaEntity>> getByIngredient(@PathVariable String ingredient) {
+        return ResponseEntity.ok(this.pizzaService.getWith(ingredient));
+    }
+
+    @GetMapping("/without/{ingredient}")
+    public ResponseEntity<List<PizzaEntity>> getByNotContainIngredient(@PathVariable String ingredient) {
+        return ResponseEntity.ok(this.pizzaService.getWithout(ingredient));
+    }
+
     @PostMapping
     public ResponseEntity<PizzaEntity> add(@RequestBody PizzaEntity pizza) {
 
