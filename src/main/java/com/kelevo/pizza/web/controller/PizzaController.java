990d68e1d4b170b0d1c03d405c5e7cb077c281f4
Patrick Jhonatan Hernandez Blanco <patrickjhonatanh@gmail.com>
2025-09-29 05:28:52 -0600

    Filter and sorting with data JPA

--- src/main/java/com/kelevo/pizza/web/controller/PizzaController.java
+++ src/main/java/com/kelevo/pizza/web/controller/PizzaController.java
@@ -45,6 +45,11 @@ public class PizzaController {
         return ResponseEntity.ok(this.pizzaService.getWithout(ingredient));
     }
 
+    @GetMapping("/cheapest/{price}")
+    public ResponseEntity<List<PizzaEntity>> getCheapest(@PathVariable double price) {
+        return ResponseEntity.ok(this.pizzaService.getCheapest(price));
+    }
+
     @PostMapping
     public ResponseEntity<PizzaEntity> add(@RequestBody PizzaEntity pizza) {
 
