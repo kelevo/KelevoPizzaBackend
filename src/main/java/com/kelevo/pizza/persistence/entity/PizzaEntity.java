ece099fbc2d6f1c564aef7934d9682fd3fc6eaa2
Patrick Jhonatan Hernandez Blanco <patrickjhonatanh@gmail.com>
2025-10-03 18:37:07 -0600

    Entity auditory for pizza

--- src/main/java/com/kelevo/pizza/persistence/entity/PizzaEntity.java
+++ src/main/java/com/kelevo/pizza/persistence/entity/PizzaEntity.java
@@ -6,12 +6,15 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import java.time.LocalDateTime;
+
 @Entity
 @Table(name = "pizza")
+@EntityListeners(AuditingEntityListener.class)
 @Getter
 @Setter
 @NoArgsConstructor
-public class PizzaEntity {
+public class PizzaEntity extends AuditableEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
