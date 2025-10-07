084b9cb91c48aa619cbd2557a1f4058e1f3d48a1
Patrick Jhonatan Hernandez Blanco <patrickjhonatanh@gmail.com>
2025-10-07 11:42:29 -0600

    Create userEntity and insert data

---
+++ src/main/java/com/kelevo/pizza/persistence/entity/UserEntity.java
@@ -0,0 +1,36 @@
+package com.kelevo.pizza.persistence.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "user")
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class UserEntity {
+
+    @Id
+    @Column(nullable = false, length = 20)
+    private String username;
+
+    @Column(unique = false, length = 200)
+    private String password;
+
+    @Column(length = 50)
+    private String email;
+
+    @Column(nullable = false, columnDefinition = "TINYINT")
+    private Boolean locked;
+
+    @Column(nullable = false, columnDefinition = "TINYINT")
+    private Boolean disabled;
+
+}
