0c149c9d054eff9f253985c49c2d556ac6904440
Patrick Jhonatan Hernandez Blanco <patrickjhonatanh@gmail.com>
2025-10-11 10:52:58 -0600

    Validate JWT with Auth0

--- src/main/java/com/kelevo/pizza/web/config/JWTUtils.java
+++ src/main/java/com/kelevo/pizza/web/config/JWTUtils.java
@@ -2,6 +2,7 @@ package com.kelevo.pizza.web.config;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
@@ -20,6 +21,19 @@ public class JWTUtils {
                 .withIssuedAt(new Date())
                 .withExpiresAt(new Date(System.currentTimeMillis() + TimeUnit.DAYS.toMillis(15)))
                 .sign(ALGORITHM);
+    }
+
+    public boolean isValid(String jwt) {
+        try {
+            JWT.require(ALGORITHM).build().verify(jwt);
+            return true;
+        } catch(JWTVerificationException ex) {
+            return false;
+        }
+    }
+
+    public String getUsername(String jwt) {
+        return JWT.require(ALGORITHM).build().verify(jwt).getSubject();
     }
 
 }
