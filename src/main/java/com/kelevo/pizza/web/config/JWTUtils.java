93031a51f4d488a2a98eecd274e2da3b96e78eb1
Patrick Jhonatan Hernandez Blanco <patrickjhonatanh@gmail.com>
2025-10-11 00:26:46 -0600

    Implement JWT and initial configuration

---
+++ src/main/java/com/kelevo/pizza/web/config/JWTUtils.java
@@ -0,0 +1,25 @@
+package com.kelevo.pizza.web.config;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+@Component
+public class JWTUtils {
+
+    private static String SECRET_KEY = "kelevo_pizz4"
+    private static Algorithm ALGORITHM = Algorithm.HMAC256(SECRET_KEY);
+
+    public String create(String username) {
+        return JWT.create()
+                .withSubject(username)
+                .withIssuer("kelevo-pizza")
+                .withIssuedAt(new Date())
+                .withExpiresAt(new Date(System.currentTimeMillis() + TimeUnit.DAYS.toMillis(15)))
+                .sign(ALGORITHM);
+    }
+
+}
